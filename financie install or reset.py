e16bbbbb092a8a59955205e7023ebba7d754730e
Boris Heinrich <boborko040404@gmail.com>
2018-06-09 18:55:42 +0200

    Prvý Upload
    2 súbory
    - inštalačný
    - financie.py - bez updatu a zápisu do súboru

---
+++ financie install or reset.py
@@ -0,0 +1,17 @@
+#financie instalacia/reset.py
+import os
+file = open("financie","w")
+file.write("0.05\n0\n")
+file.write("0.10\n0\n")
+file.write("0.20\n0\n")
+file.write("0.50\n0\n")
+file.write("1.00\n0\n")
+file.write("2.00\n0\n")
+file.write("5.00\n0\n")
+file.write("10.00\n0\n")
+file.write("20.00\n0\n")
+file.write("50.00\n0\n")
+file.write("100.00\n0\n")
+file.write("200.00\n0\n")
+file.write("500.00\n0")
+file.close()
---
+++ financie.py
@@ -0,0 +1,41 @@
+import os
+file = open("financie","r+")
+a = 26
+b = 1
+c = 2
+f = 0
+suma = 0
+for x in range(14):
+    typ = float(file.readline(b))
+    if typ == 5.0 and f == 0:
+        typ = 0.05
+        f = 1
+    hodnota = float(file.readline(c))
+    hodnota = hodnota / 10
+    suma = float(suma + typ * hodnota)
+    hodnota = int(hodnota)
+    hodnota = str(hodnota)
+    if hodnota != "0":
+        typ = str(typ)
+        print(typ + "€ -> " + hodnota)
+    b = b + 2
+    c = c + 2
+file.close()
+suma = str(suma)
+print("Momentalne máš: " + suma + "€")
+print("\nChceš editovať?")
+print("[0] NIE\n[1] Pridať\n[2] Odobrať")
+d = int(input())
+if d == 1:
+    file = open("financie","r+")
+    file2 = open("financie2","w")
+    print("Zadaj hodnotu, ktorú chceš pridať (0.10|2.00)")
+    o = float(input())
+    print("Zadaj koľkokrát chceš hodnotu pridať (1|5)")
+    p = str(input())
+    a = file.readlines()
+file.close()
+file2.close()
+os.remove("financie")
+os.rename('financie2', 'financie')
+input()
